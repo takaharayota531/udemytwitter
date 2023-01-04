517e70355c4bfa4f96ca99d3c4961bfd76dc58a8
takaharayota531 <takahara.yota@gmail.com>
2023-01-04 13:44:57 +0900

    tmp_commit

---
+++ lib/details/rounded_text_field.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RoundedTextField extends StatelessWidget {
+  const RoundedTextField({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
